441badf79ce734988263a20e8bca9e4d8eb7a8ff
HuangCheng72 <huangcheng20000702@gmail.com>
2024-05-21 16:42:13 +0800

    配置nasm、bochs、dd环境，添加一键编译烧写脚本

---
+++ Program/w.bat
@@ -0,0 +1,33 @@
+@echo off
+
+rem 先执行make操作
+make all
+
+setlocal
+
+rem 确定mbr.bin文件的路径
+set MBR_BIN=%~dp0boot\mbr.bin
+
+rem 确定目标硬盘映像文件的相对路径
+set HD_IMG_REL=..\VM\HOS.raw
+
+rem 计算目标硬盘映像文件的绝对路径
+for %%I in (%HD_IMG_REL%) do set HD_IMG=%%~fI
+
+rem 输出涉及各文件的路径信息
+echo MBR_BIN=%MBR_BIN%
+echo HD_IMG=%HD_IMG%
+
+rem 切换到脚本所在的目录
+cd /d %~dp0
+
+rem 首先清空硬盘
+dd if=/dev/zero of=%HD_IMG% bs=1M count=64 conv=notrunc
+rem 使用dd将mbr.bin写入硬盘映像文件
+dd if=%MBR_BIN% of=%HD_IMG% bs=512 count=1 conv=notrunc
+
+rem 执行clean操作
+make clean
+
+endlocal
+pause
