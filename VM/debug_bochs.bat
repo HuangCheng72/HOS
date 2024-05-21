0f5b1e53b508722232bc29eb81a2bac2db971a05
HuangCheng72 <huangcheng20000702@gmail.com>
2024-05-22 02:04:12 +0800

    启用调试的脚本

---
+++ VM/debug_bochs.bat
@@ -0,0 +1,12 @@
+@echo off
+REM 检查当前目录下是否存在Configuration.bxrc文件
+if exist Configuration.bxrc (
+    
+    REM 使用绝对路径启动Bochsdbg
+    bochsdbg -q -f %~dp0Configuration.bxrc
+    
+) else (
+    REM 如果不存在Configuration.bxrc文件，输出提示信息并退出
+    echo Configuration.bxrc文件不存在，程序将退出。
+    exit /b
+)
