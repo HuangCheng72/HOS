a2dc0063935c588db1e2565398c18c40be3c322b
HuangCheng72 <huangcheng20000702@gmail.com>
2024-05-28 15:22:47 +0800

    实现位图数据结构

---
+++ Program/lib/lib_kernel/bitmap/bitmap.h
@@ -0,0 +1,26 @@
+//
+// Created by huangcheng on 2024/5/28.
+//
+
+#ifndef HOS_BITMAP_H
+#define HOS_BITMAP_H
+
+#include "../../type.h"
+
+typedef struct {
+    // 位图长度（字节数）
+    uint32_t btmp_bytes_len;
+    // 位图起始地址
+    uint8_t* bits;
+}BitMap;
+
+// 位图初始化
+void bitmap_init(BitMap* btmp);
+// 判断bit_idx位是否为1（即已经被设置）
+int32_t bitmap_is_bit_set(BitMap* btmp, uint32_t bit_idx);
+// 将位图btmp的bit_idx位设置为value
+void bitmap_set(BitMap* btmp, uint32_t bit_idx, int8_t value);
+// 在位图中申请连续cnt个位,成功则返回其起始位下标，失败返回-1
+int32_t bitmap_scan(BitMap* btmp, uint32_t cnt);
+
+#endif //HOS_BITMAP_H
