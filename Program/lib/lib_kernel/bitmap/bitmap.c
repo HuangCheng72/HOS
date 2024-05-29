ca00f2f34ff71050fdddccc0aa8fc2f01c6e086e
HuangCheng72 <huangcheng20000702@gmail.com>
2024-05-29 23:44:45 +0800

    位图修改

--- Program/lib/lib_kernel/bitmap/bitmap.c
+++ Program/lib/lib_kernel/bitmap/bitmap.c
@@ -55,7 +55,47 @@ void bitmap_set(BitMap* btmp, uint32_t bit_idx, int8_t value) {
 int32_t bitmap_scan(BitMap* btmp, uint32_t cnt) {
     uint32_t idx_byte = 0;	 // 用于记录空闲位所在的字节
     // 首先逐个字节比较，如果值为0xff（255）意味着全部占满，那就没必要检测了，直接计数跳过即可
-    while (( 0xff == btmp->bits[idx_byte]) && (idx_byte < btmp->btmp_bytes_len)) {
+    while ((idx_byte < btmp->btmp_bytes_len) && ( 0xff == btmp->bits[idx_byte])) {
+        idx_byte++;
+    }
+
+    if (idx_byte >= btmp->btmp_bytes_len) {
+        // 若找不到可用空间，那就真没办法了
+        return -1;
+    }
+
+    // 找到一个空闲位的字节，逐位比较，查找连续的空闲位
+    int32_t bit_idx_start = -1;
+    uint32_t count = 0;
+    uint32_t bit_left = btmp->btmp_bytes_len * 8;  // 总位数
+
+    for (uint32_t i = idx_byte * 8; i < bit_left; i++) {
+        if ( bitmap_is_bit_set(btmp, i) ) {
+            // 还没找到足够的空位就被中断了，重置计数器和起点
+            bit_idx_start = -1;
+            count = 0;
+
+        } else {
+            // 找到第一个空闲位
+            if (bit_idx_start == -1) {
+                bit_idx_start = i;
+            }
+            count++;
+            if (count == cnt) {
+                return bit_idx_start;  // 找到连续的cnt个空闲位
+            }
+
+        }
+    }
+    return -1;  // 找不到连续的cnt个空闲位
+}
+
+int32_t bitmap_scan_from_start(BitMap* btmp, uint32_t start_bit_idx, uint32_t cnt){
+
+    uint32_t idx_byte = start_bit_idx / 8; // 从起点所在的字节开始
+
+    // 首先逐个字节比较，如果值为0xff（255）意味着全部占满，那就没必要检测了，直接计数跳过即可
+    while ((idx_byte < btmp->btmp_bytes_len) && ( 0xff == btmp->bits[idx_byte])) {
         idx_byte++;
     }
 
