cc8ca571b3f9053fd8cd281adc01852aa48eaacc
HuangCheng72 <huangcheng20000702@gmail.com>
2024-05-31 13:21:45 +0800

    建立设备驱动框架

--- Program/kernel/kernel_device/kernel_device.c
+++ Program/kernel/kernel_device/kernel_device.c
@@ -3,9 +3,68 @@
 //
 
 #include "kernel_device.h"
-#include "pit8253/pit8253.h"
+
+// 驱动链表头结点
+struct list_node driver_list;
+
+// 驱动段的起点和终点
+extern struct driver __drivers_start[];
+extern struct driver __drivers_end[];
 
 void init_device() {
-    // 初始化内核必需的设备
-    init_pit8253();
+    // 初始化驱动链表头结点
+    init_list_node(&driver_list);
+    // 链接到驱动链表
+    struct driver* drv;
+    for (drv = __drivers_start; drv < __drivers_end; drv++) {
+        driver_add(drv);
+    }
+}
+void exit_device() {
+    // 移除所有设备
+    struct list_node *pos, *n;
+    list_for_each_safe(pos, n, &driver_list) {
+        // 执行退出程序，然后从链表中移除
+        if(list_entry(pos, struct driver, node)->exit != NULL) {
+            list_entry(pos, struct driver, node)->exit();
+        }
+        list_del(pos);
+        return;
+    }
+    // 初始化驱动链表头结点
+    init_list_node(&driver_list);
+}
+
+// 添加单个驱动
+void driver_add(struct driver* drv) {
+    if(drv == NULL) {
+        return;
+    }
+    // 防止重复注册
+    struct list_node *pos, *n;
+    list_for_each_safe(pos, n, &driver_list) {
+        if (strcmp(list_entry(pos, struct driver, node)->driver_name, drv->driver_name) == 0) {
+            return;
+        }
+        put_str(drv->driver_name);
+        put_char('\n');
+    }
+    list_add_tail(&drv->node, &driver_list);
+    if(drv->init != NULL) {
+        drv->init();
+    }
+}
+// 移除单个驱动
+void driver_remove(const char *driver_name) {
+    struct list_node *pos, *n;
+    list_for_each_safe(pos, n, &driver_list) {
+        if(strcmp(list_entry(pos, struct driver, node)->driver_name, driver_name) == 0) {
+            // 执行退出程序，然后从链表中移除
+            if(list_entry(pos, struct driver, node)->exit != NULL) {
+                list_entry(pos, struct driver, node)->exit();
+            }
+            list_del(pos);
+            return;
+        }
+    }
 }
--- Program/kernel/kernel_device/kernel_device.h
+++ Program/kernel/kernel_device/kernel_device.h
@@ -5,7 +5,34 @@
 #ifndef HOS_KERNEL_DEVICE_H
 #define HOS_KERNEL_DEVICE_H
 
-// 初始化内核必需的设备
+#include "../../lib/lib_kernel/lib_kernel.h"
+#include "../kernel_interrupt/kernel_interrupt.h"
+#include "../kernel_idt/kernel_idt.h"
+
+// 统一的驱动接口结构体（这个结构体的实例一律放在驱动段）
+struct driver {
+    char driver_name[64];                       // 驱动名称
+    void (*init)(void);                         // 初始化函数
+    void (*exit)(void);                         // 退出函数
+    void (*interrupt_handler)(void);            // 中断处理函数（可选）
+    int (*read)(void* buffer, int size);        // 读操作（可选）
+    int (*write)(const void* buffer, int size); // 写操作（可选）
+    struct list_node node;                      // 用于串联到驱动链表上的链表结点
+};
+
+// 初始化所有设备
 void init_device();
+// 移除所有设备
+void exit_device();
+// 添加单个驱动并初始化
+void driver_add(struct driver* drv);
+// 移除单个驱动并卸载
+void driver_remove(const char *driver_name);
+
+// 这个注册宏的作用就是把数据放在驱动段（而且告诉编译器这个是被使用的，不能优化掉），它会定义一个驱动结构体实例
+#define REGISTER_DRIVER(drv) \
+    __attribute__((section(".drivers"), used)) struct driver drv =
+
+
 
 #endif //HOS_KERNEL_DEVICE_H
