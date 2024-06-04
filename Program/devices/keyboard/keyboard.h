51b2795042682488e0d556f7618bb86d9950041b
HuangCheng72 <huangcheng20000702@gmail.com>
2024-06-05 02:31:51 +0800

    键盘驱动实现

---
+++ Program/devices/keyboard/keyboard.h
@@ -0,0 +1,10 @@
+//
+// Created by huangcheng on 2024/6/4.
+//
+
+#ifndef HOS_KEYBOARD_H
+#define HOS_KEYBOARD_H
+
+void keyboard_init();
+
+#endif //HOS_KEYBOARD_H
--- Program/kernel/kernel.c
+++ Program/kernel/kernel.c
@@ -5,10 +5,11 @@
 #include "kernel_device/kernel_device.h"
 #include "kernel_memory/kernel_memory.h"
 #include "kernel_task/kernel_task.h"
+#include "../devices/console/console.h"
+#include "kernel_buffer//kernel_buffer.h"
 
-/* 两个都是在线程中运行的函数 */
-void k_thread_a(void* arg);
-void k_thread_b(void* arg);
+// 键盘监视任务（实际上监视对象是分配给键盘的缓冲区）
+void keyboard_task();
 
 void kernel_main(void) {
 
@@ -23,44 +24,38 @@ void kernel_main(void) {
     init_idt();
     // 初始化内存管理
     init_memory(total_physical_memory);
-    // 初始化所有设备
-    init_all_devices();
     // 初始化线程相关结构
     init_multitasking();
+    // 初始化所有设备
+    init_all_devices();
 
-    task_create("k_thread_a", 31, k_thread_a, "argA ");
-    task_create("k_thread_b", 8, k_thread_b, "argB ");
+
+    // 开一个键盘监视任务，不然就要用内核主任务监视了
+    task_create("keyboard_task", 31, keyboard_task, NULL);
 
     // 允许PIC_IRQ0中断，才可以让定时器调度线程
     enable_interrupt(0);
+    // 允许PIC_IRQ1中断，才可以处理键盘输入
+    enable_interrupt(1);
     // 开启全局中断
     intr_enable();
 
     // 进入内核主循环或其它初始化代码
     for(;;) {
-        put_str("MAIN");
-        for(uint32_t i = 0; i < UINT16_MAX; i++);
+
     }
     // 退出主循环卸载设备驱动
     exit_all_devices();
 }
 
-/* 在线程中运行的函数 */
-void k_thread_a(void* arg) {
-/* 用void*来通用表示参数,被调用的函数知道自己需要什么类型的参数,自己转换再用 */
-    char* para = arg;
-    while(1) {
-        put_str(para);
-        for(uint32_t i = 0; i < UINT16_MAX; i++);
-    }
-}
-
-/* 在线程中运行的函数 */
-void k_thread_b(void* arg) {
-/* 用void*来通用表示参数,被调用的函数知道自己需要什么类型的参数,自己转换再用 */
-    char* para = arg;
-    while(1) {
-        put_str(para);
-        for(uint32_t i = 0; i < UINT16_MAX; i++);
+// 键盘监视任务（实际上监视对象是分配给键盘的缓冲区）
+void keyboard_task() {
+    struct driver *keyboard_driver = get_driver("keyboard");
+    char data;
+    for(;;) {
+        if(device_read(keyboard_driver, &data, 1) > 0) {
+            // 有数据就全部打到屏幕上
+            console_put_char(data);
+        }
     }
 }
