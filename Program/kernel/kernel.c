12b68cc8ddd076d1c570f1875cbf70c732b3fdb7
HuangCheng72 <huangcheng20000702@gmail.com>
2024-06-09 17:42:09 +0800

    提交文档

--- Program/kernel/kernel.c
+++ Program/kernel/kernel.c
@@ -6,10 +6,10 @@
 #include "kernel_memory/kernel_memory.h"
 #include "kernel_task/kernel_task.h"
 #include "../devices/console/console.h"
-#include "kernel_buffer//kernel_buffer.h"
+#include "kernel_buffer/kernel_buffer.h"
+#include "../user/process/process.h"
 
-// 键盘监视任务（实际上监视对象是分配给键盘的缓冲区）
-void keyboard_task();
+#include "../lib/lib_user/syscall.h"
 
 void kernel_main(void) {
 
@@ -22,23 +22,15 @@ void kernel_main(void) {
     switch_esp_virtual_addr();
     // 初始化idt
     init_idt();
+    // 先初始化任务管理结构（这样所有任务就能用统一的内存管理逻辑了）
+    init_multitasking();
     // 初始化内存管理
     init_memory(total_physical_memory);
-    // 初始化线程相关结构
-    init_multitasking();
     // 初始化所有设备
     init_all_devices();
 
-
-    // 开一个键盘监视任务，不然就要用内核主任务监视了
-    task_create("keyboard_task", 31, keyboard_task, NULL);
-
-    // 允许PIC_IRQ0中断，才可以让定时器调度线程
-    enable_interrupt(0);
-    // 允许PIC_IRQ1中断，才可以处理键盘输入
-    enable_interrupt(1);
-    // 开启全局中断
-    intr_enable();
+    // 屏蔽所有中断
+    intr_disable();
 
     // 进入内核主循环或其它初始化代码
     for(;;) {
@@ -46,16 +38,4 @@ void kernel_main(void) {
     }
     // 退出主循环卸载设备驱动
     exit_all_devices();
-}
-
-// 键盘监视任务（实际上监视对象是分配给键盘的缓冲区）
-void keyboard_task() {
-    struct driver *keyboard_driver = get_driver("keyboard");
-    char data;
-    for(;;) {
-        if(device_read(keyboard_driver, &data, 1) > 0) {
-            // 有数据就全部打到屏幕上
-            console_put_char(data);
-        }
-    }
 }
