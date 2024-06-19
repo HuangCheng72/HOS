bf9f664024279ee9ae0b1129d5e62c0f69249a94
HuangCheng72 <huangcheng20000702@gmail.com>
2024-06-19 09:56:16 +0800

    栈提高到0x7000为栈底，栈内存充足

--- Program/lib/lib_kernel/switch_esp_virtual_addr.asm
+++ Program/lib/lib_kernel/switch_esp_virtual_addr.asm
@@ -11,14 +11,14 @@ switch_esp_virtual_addr:
     sub ebx, eax            ; 计算数据块大小（ebx = 0x0900 - 当前 ESP）
 
     ; Step 2: 搬移数据
-    ; 数据源是esp到0x900（不包括0x900处），目的地是esp + 0xc00 到 0x1500（不包括0x1500）
+    ; 数据源是esp到0x900（不包括0x900处），目的地是esp + 0x6700 到 0x7000（不包括0x7000）
     mov esi, eax            ; 源地址为当前 ESP
-    add eax, 0x0c00         ; 新栈顶地址
+    add eax, 0x6700         ; 新栈顶地址
     mov edi, eax            ; 目标地址为新 ESP
     mov ecx, ebx            ; 设置需要复制的字节数
     cld                     ; 清除方向标志位，确保数据从低地址到高地址搬移
     rep movsb               ; 复制数据块到新位置
 
     ; Step 3: 更新 ESP 到新的虚拟地址
-    add esp, 0xc0000c00     ; 切换 ESP 到高地址（esp = 当前 esp + 0xC0000C00）
+    add esp, 0xc0006700     ; 切换 ESP 到高地址（esp = 当前 esp + 0xC0006700）
     ret                     ; 返回
