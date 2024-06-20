92180819988c040634e234a9f9b96a38d981441e
HuangCheng72 <huangcheng20000702@gmail.com>
2024-06-21 07:34:22 +0800

    ramdisk恢复原样

--- Program/devices/ramdisk/ramdisk.c
+++ Program/devices/ramdisk/ramdisk.c
@@ -22,28 +22,10 @@ REGISTER_DRIVER(ramdisk_driver) {
 static uint8_t *ramdisk = (uint8_t *)0xA0000000; // 定义RAM Disk的存储空间
 
 struct ramdisk_io_request {
-    uint32_t address; // 以字节为单位的地址（nor闪存没有扇区的概念，直接以线性地址读写）
+    uint32_t address; // 以字节为单位的地址
     uint32_t size;
     char *buffer;
 };
-
-// 擦除块，这是nor闪存特有的操作，nor闪存写之前，必须要擦除该块，然后才能写
-int32_t erase_block(uint32_t block_number) {
-    if (block_number * BLOCK_SIZE >= RAMDISK_SIZE) {
-        return -1; // 超出范围
-    }
-    memset(ramdisk + block_number * BLOCK_SIZE, 0xFF, BLOCK_SIZE); // 模拟NOR闪存的擦除操作（一次性擦除一个块，就是每一位都用1填充）
-    return 0;
-}
-
-// 文件系统的擦除要求是4KB，适配
-int32_t erase_block_4K(uint32_t addr) {
-    if(addr > RAMDISK_SIZE - 0x1000) {
-        return -1;
-    }
-    memset((char *)addr, 0xff, 0x1000);
-    return 0;
-}
 
 int32_t ramdisk_read(char *args, uint32_t args_size) {
     if(args_size != sizeof(struct ramdisk_io_request)) {
@@ -67,9 +49,6 @@ int32_t ramdisk_write(char *args, uint32_t args_size) {
     if (request->address + request->size > RAMDISK_SIZE) {
         return -1; // 超出范围
     }
-
-    // 不擦除了，直接写
     memcpy(ramdisk + request->address, request->buffer, request->size);
-
     return request->size;
 }
--- Program/devices/ramdisk/ramdisk.h
+++ Program/devices/ramdisk/ramdisk.h
@@ -7,17 +7,8 @@
 
 #include "../../lib/lib_kernel/lib_kernel.h"
 
-// 用ramdisk模拟nor闪存的行为
-
-
 #define RAMDISK_SIZE (16 * 1024 * 1024) // 16MB
-#define BLOCK_SIZE (64 * 1024) // NOR闪存块大小，通常为64KB
-#define WRITE_GRANULARITY 4 // 写入粒度为4字节（一个字）
 
-// 擦除块操作
-int32_t erase_block(uint32_t block_number);
-// 粒度4KB的擦除块操作
-int32_t erase_block_4K(uint32_t addr);
 int32_t ramdisk_read(char *args, uint32_t args_size);
 int32_t ramdisk_write(char *args, uint32_t args_size);
 
