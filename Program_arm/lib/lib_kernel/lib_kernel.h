2c4f5aba2a38fa3e9154ca9859af3bc872e1efb1
HuangCheng72 <huangcheng20000702@gmail.com>
2024-07-03 02:30:01 +0800

    页表操作、切换栈指针

--- Program_arm/lib/lib_kernel/lib_kernel.h
+++ Program_arm/lib/lib_kernel/lib_kernel.h
@@ -7,15 +7,18 @@
 
 #include "../type.h"
 #include "print/print.h"
+#include "string/string.h"
+#include "bitmap/bitmap.h"
+#include "list/list.h"
 
 // 这个文件存在的意义主要就是为了声明汇编暴露的函数，加上type.h可以为系统各组件提供统一的支持
 
-// 载入页目录表，paging_ops.asm
+// 载入页目录表到ttbr0和ttbr1，paging_ops.asm
 extern void load_page_directory(uint32_t page_directory_addr);
-// 启用分页，paging_ops.asm
-extern void enable_paging();
-// 获取cr3寄存器中的当前页表，paging_ops.asm
+// 获取TTBR0寄存器中的当前页表，paging_ops.asm
 extern uint32_t get_current_page(void);
+// 切换栈指针
+extern void switch_sp(uint32_t new_sp);
 
 
 #endif //HOS_LIB_H
--- Program_arm/lib/lib_kernel/paging_ops.asm
+++ Program_arm/lib/lib_kernel/paging_ops.asm
@@ -1,26 +1,98 @@
 .section .text
 
-.global enable_paging
 .global load_page_directory
 .global get_current_page
+.global get_ttbcr
+.global set_ttbcr
+.global get_sctlr
+.global set_sctlr
+.global get_dacr
+.global set_dacr
+.global invalidate_tlbs
 
-// void enable_paging();
-enable_paging:
-    mrc p15, 0, r0, c1, c0, 0   // 读取System Control Register (SCTLR)
-    orr r0, r0, #0x1            // 设置第0位（M位）启用分页
-    mcr p15, 0, r0, c1, c0, 0   // 写回SCTLR
-    bx lr
+// x86的C调用约定完全通过栈传参
+// arm不一样，前四个参数通过r0到r3四个寄存器传参，多余的参数通过栈传参
+// 第一个参数就是r0
+// 所以直接加载就行了
 
 // void load_page_directory(uint32_t page_directory_addr);
 load_page_directory:
-    // 从栈获取参数
-    push {r4, lr}                // 保存寄存器
-    ldr r0, [sp, #8]             // 获取第一个参数
-    mcr p15, 0, r0, c2, c0, 0    // 设置页表基地址寄存器（TTBR0）
-    pop {r4, lr}                 // 恢复寄存器
-    bx lr
+    // 设置TTBR0
+    mcr p15, 0, r0, c2, c0, 0    // 设置TTBR0，r0包含页表基地址
+    // 设置TTBR1
+    mcr p15, 0, r0, c2, c0, 1    // 设置TTBR1，r0包含页表基地址
+
+    bx lr                        // 返回调用者
 
 // uint32_t get_current_page(void);
 get_current_page:
     mrc p15, 0, r0, c2, c0, 0    // 读取TTBR0寄存器
+    bx lr
+
+// 获取TTBCR寄存器的值并存储到结构体中
+// void get_ttbcr(TTBCR_t *ttbcr);
+get_ttbcr:
+    // 读取TTBCR寄存器的值到r1
+    mrc p15, 0, r1, c2, c0, 2
+    // 将r1的值存储到结构体中
+    str r1, [r0]
+    // 返回
+    bx lr
+
+// 从结构体中读取并设置TTBCR寄存器的值
+// void set_ttbcr(TTBCR_t *ttbcr);
+set_ttbcr:
+    // 从结构体中加载值到r1
+    ldr r1, [r0]
+    // 将r1的值写入TTBCR寄存器
+    mcr p15, 0, r1, c2, c0, 2
+    // 返回
+    bx lr
+
+// 获取SCTLR寄存器的值并存储到结构体中
+// void get_sctlr(SCTLR_t *sctlr);
+get_sctlr:
+    // 读取SCTLR寄存器的值到r1
+    mrc p15, 0, r1, c1, c0, 0
+    // 将r1的值存储到结构体中
+    str r1, [r0]
+    // 返回
+    bx lr
+
+// 从结构体中读取并设置SCTLR寄存器的值
+// void set_sctlr(SCTLR_t *sctlr);
+set_sctlr:
+    // 从结构体中加载值到r1
+    ldr r1, [r0]
+    // 将r1的值写入SCTLR寄存器
+    mcr p15, 0, r1, c1, c0, 0
+    // 返回
+    bx lr
+
+// 获取DACR寄存器的值并存储到结构体中
+// void get_dacr(DACR_t *dacr);
+get_dacr:
+    // 读取DACR寄存器的值到r1
+    mrc p15, 0, r1, c3, c0, 0
+    // 将r1的值存储到结构体中
+    str r1, [r0]
+    // 返回
+    bx lr
+
+// 从结构体中读取并设置DACR寄存器的值
+// void set_dacr(DACR_t *dacr);
+set_dacr:
+    // 从结构体中加载值到r1
+    ldr r1, [r0]
+    // 将r1的值写入DACR寄存器
+    mcr p15, 0, r1, c3, c0, 0
+    // 返回
+    bx lr
+
+// 刷新TLB，使TLB无效
+invalidate_tlbs:
+    mov r2, #0
+    mcr p15, 0, r2, c8, c7, 0    // 使TLB无效
+    //mcr p15, 0, r2, c7, c10, 4   // 数据同步屏障（DSB）
+    dsb
     bx lr
