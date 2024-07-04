764e9ae4c109b77875b79f3d252c0c77e4ff8473
HuangCheng72 <huangcheng20000702@gmail.com>
2024-07-04 17:12:30 +0800

    移植x86上的内存管理

--- Program_arm/kernel/kernel.c
+++ Program_arm/kernel/kernel.c
@@ -4,6 +4,7 @@
 
 #include "../lib/lib_kernel/lib_kernel.h"
 #include "kernel_page/kernel_page.h"
+#include "kernel_task/kernel_task.h"
 
 void kernel_main(void) {
     // u-boot已经给我们设置了管理模式
@@ -17,6 +18,10 @@ void kernel_main(void) {
 
     // 切换栈指针到虚拟地址，这样就可以顺利使用内核栈了
     switch_sp(0xc0007000);
+    // 初始化task
+    init_multitasking();
+    // 初始化内存管理（本来这里应该从环境里面获取内存大小，我懒，直接写死了算了）
+    init_memory(0x8000000);
 
     for(;;);
 }
